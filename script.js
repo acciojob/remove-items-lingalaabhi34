0b308193a1debe002e127632f9fcceef9cb41463
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-29 12:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function remove(){
+    const select = document.querySelector("#colorSelect");
+    let selectedvalue = select.value;
+    let options = select.children;
+    for(let i=0;i<options.length;i++){
+        options[i].value === selectedvalue;
+        options[i].remove();
+        return;
+    }
+}
