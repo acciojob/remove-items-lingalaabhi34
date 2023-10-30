dc3bdc47fa2b5a45b5701020263950d8fc65b218
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-30 11:11:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 const button = document.querySelector("input");
 
-let colorselect = document.querySelector("#colorSelect");
+let colorSelect = document.querySelector("#colorSelect");
 button.addEventListener("click",()=>{
-    let selected = colorselect.value;
-    for(let i=0;i<colorselect.options.length;i++){
-        if(colorselect.options[i].value === selected){
-            colorselect.remove(i);
+    let selected = colorSelect.value;
+    for(let i=0;i<colorSelect.options.length;i++){
+        if(colorSelect.options[i].value === selected){
+            colorSelect.remove(i);
             return;
         }
     }
